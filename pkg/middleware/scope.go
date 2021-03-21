6b17dc0736437c8242242beff1398aab71d7972a
Nick Meves <nick.meves@greenhouse.io>
2021-03-20 19:09:11 -0700

    Move RequestID management to RequestScope

--- pkg/middleware/scope.go
+++ pkg/middleware/scope.go
@@ -3,20 +3,28 @@ package middleware
 import (
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/justinas/alice"
 	middlewareapi "github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/middleware"
-	requestutil "github.com/oauth2-proxy/oauth2-proxy/v7/pkg/requests/util"
 )
 
-func NewScope(reverseProxy bool) alice.Constructor {
+func NewScope(reverseProxy bool, idHeader string) alice.Constructor {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			scope := &middlewareapi.RequestScope{
 				ReverseProxy: reverseProxy,
-				RequestID:    requestutil.GenRequestID(req),
+				RequestID:    genRequestID(req, idHeader),
 			}
 			req = middlewareapi.AddRequestScope(req, scope)
 			next.ServeHTTP(rw, req)
 		})
 	}
+}
+
+func genRequestID(req *http.Request, idHeader string) string {
+	rid := req.Header.Get(idHeader)
+	if rid != "" {
+		return rid
+	}
+	return uuid.New().String()
 }
